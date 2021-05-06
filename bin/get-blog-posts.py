492a2fa534a2c6f3d553b109fd5a7d711820ea18
eunminlee <eunmin_lee@hotmail.com>
2021-05-05 22:25:11 -1000

    updated

--- bin/get-blog-posts.py
+++ bin/get-blog-posts.py
@@ -11,20 +11,20 @@ import sys
 
 NOTION_TOKEN = os.getenv('NOTION_TOKEN')
 NOTION_ROOT_PAGE_ID = os.getenv('NOTION_ROOT_PAGE_ID')
-NOTION_EMAIL = os.getenv('NOTION_EMAIL')
-NOTION_PWD = os.getenv('NOTION_PWD')
+# NOTION_EMAIL = os.getenv('NOTION_EMAIL')
+# NOTION_PWD = os.getenv('NOTION_PWD')
 if NOTION_TOKEN is None:
     sys.exit("The NOTION_TOKEN is missing, see the readme on how to set it.")
 if NOTION_ROOT_PAGE_ID is None:
     sys.exit("The NOTION_ROOT_PAGE_ID is missing, see the readme on how to set it.")
-if NOTION_EMAIL is None:
-    sys.exit("The NOTION_EMAIL is missing, see the readme on how to set it.")
-if NOTION_PWD is None:
-    sys.exit("The NOTION_PWD is missing, see the readme on how to set it.")
+# if NOTION_EMAIL is None:
+#     sys.exit("The NOTION_EMAIL is missing, see the readme on how to set it.")
+# if NOTION_PWD is None:
+#     sys.exit("The NOTION_PWD is missing, see the readme on how to set it.")
 
 
 
-client = NotionClient(token_v2=NOTION_TOKEN, email=NOTION_EMAIL, password=NOTION_PWD)
+client = NotionClient(token_v2=NOTION_TOKEN)
 root_page_id = NOTION_ROOT_PAGE_ID
 
 dest_path = os.path.normpath(os.path.join(
