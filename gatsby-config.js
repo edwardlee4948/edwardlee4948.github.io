16f7aa856fb09b2d8387db3cd8e3c4c3ce3b50b4
eunminlee <eunmin_lee@hotmail.com>
2022-05-29 10:27:29 -1000

    updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
           // Avoids sending pageview hits from custom paths
           exclude: ["/preview/**", "/do-not-track/me/too/"],
           // Defaults to https://www.googletagmanager.com
-          origin: "www.eelvlee.com",
+          origin: "https://www.eelvlee.com",
         },
       },
     },
